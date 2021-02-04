d47918386cebb98f1745d4053e78af97d42f6caa
Katarina-Antesevic <34368270+Katarina-Antesevic@users.noreply.github.com>
2021-02-04 12:59:50 +0100

    Delete Cinema directory

--- Cinema/scripts/dodatak.sql
+++
@@ -1,5 +0,0 @@
--- korisnicki nalog
-create user 'student'@'localhost' identified by 'student';
-grant select, insert, update, delete on movie.* to 'student'@'localhost';
-grant execute on procedure movie.smanji_kapacitet_sale to 'student'@'localhost';
-flush privileges;
--- Cinema/src/org/unibl/etf/cinema/data/DataAccessFactoryType.java
+++
@@ -1,6 +0,0 @@
-package org.unibl.etf.cinema.data;
-
-public enum DataAccessFactoryType {
-
-	MySQL
-}
